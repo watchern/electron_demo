f04610708a0cc7c6d301c6cbb6295b632170b34a
watchern <watchern@126.com>
2024-11-24 12:00:07 +0800

    forge设置客户端名等

--- forge.config.js
+++ forge.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   packagerConfig: {
     asar: true,
+    name: 'xxx客户端', // 产品名称 // 文件说明
+    extraResource: ['./build/assets/readme.txt', './build/assets/img/a.png'], // 静态文件
+    executableName: 'App', // 入口可执行文件名
+    icon: './build/icons' // 不用加后缀，但是需要准备3个文件，win: icon.ico, mac: icon.icns, linux: icon.png，打包时自动识别，linux 在BrowserWindow构造参数中设置
   },
   rebuildConfig: {},
   makers: [
